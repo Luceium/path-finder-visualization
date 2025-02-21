e9c3bd423ae58010848c40395f7d449764e258b5
Mauricio Curiel <mauriciom.curiel@gmail.com>
2025-02-20 19:53:50 -0800

    add enums file

---
+++ enums.py
@@ -0,0 +1,23 @@
+from enum import Enum, auto
+
+class GridState(Enum):
+    START = auto()
+    GOAL = auto()
+    OBSTACLE = auto()
+    UNEXPLORED = auto()
+    SEEN = auto()
+    CURRENT = auto()
+
+# UI Options
+class EditMode(Enum):
+    START = 'Edit Start'
+    GOAL = 'Edit Goal'
+    OBSTACLES = 'Edit Obstacles'
+
+class Algorithm(Enum):
+    #NOTE: Switching the algorithm resets the search
+    BFS = "Breadth First Search"
+    DFS = "Depth First Search"
+    A_STAR = "A*"
+    GREEDY_BFS = "Greedy Best First Search"
+    BEAM = "Beam Search"
